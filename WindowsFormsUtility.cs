4213fa1592ae6b002bdb5923ec8096739a3d8a05
hindylauer <hindylauer@gmail.com>
2026-08-14 10:29:23 -0500

    #54

--- WindowsFormsUtility.cs
+++ WindowsFormsUtility.cs
@@ -16,7 +16,7 @@ namespace CPUWindowsFormsFramework
         }
 
 
-        public static void SetControlBinding(Control ctrl, DataTable dt)
+        public static void SetControlBinding(Control ctrl, BindingSource bindsource)
         {
             string propertyname = "";
             string controlname = ctrl.Name;
@@ -37,7 +37,7 @@ namespace CPUWindowsFormsFramework
 
             if (propertyname != "" && columnname != "")
             {
-                ctrl.DataBindings.Add(propertyname, dt, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
+                ctrl.DataBindings.Add(propertyname, bindsource, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
             }
 
         }
