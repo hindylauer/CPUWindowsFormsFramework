e96e044dae4227f34d509c2fc6a1732cf2bc1d5a
hindylauer <hindylauer@gmail.com>
2026-09-16 18:12:24 -0500

    #56

--- WindowsFormsUtility.cs
+++ WindowsFormsUtility.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Text;
+﻿using System.Data;
 
 namespace CPUWindowsFormsFramework
 {
     public class WindowsFormsUtility
     {
-        public static void SetListBinding(ComboBox lst, DataTable sourcedt, DataTable targetdt, string tablename)
+        public static void SetListBinding(ComboBox lst, DataTable sourcedt, DataTable? targetdt, string tablename)
         {
             lst.DataSource = sourcedt;
             lst.ValueMember = tablename + "Id";
             lst.DisplayMember = lst.Name.Substring(3);
-            lst.DataBindings.Add("SelectedValue", targetdt, lst.ValueMember, false, DataSourceUpdateMode.OnPropertyChanged);
+            if(targetdt != null)
+            {
+                lst.DataBindings.Add("SelectedValue", targetdt, lst.ValueMember, false, DataSourceUpdateMode.OnPropertyChanged);
+            }
         }
 
 
@@ -22,6 +22,7 @@ namespace CPUWindowsFormsFramework
             string controlname = ctrl.Name;
             string controltype = controlname.Substring(0, 3);
             string columnname = controlname.Substring(3);
+
 
             switch (controltype)
             {
@@ -42,12 +43,131 @@ namespace CPUWindowsFormsFramework
 
         }
 
-        public static void FormatGridForSearchResults(DataGridView grid)
+        public static void FormatGridForSearchResults(DataGridView grid, string tablename)
         {
             grid.AllowUserToAddRows = false;
-            grid.ReadOnly = true;
-            grid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            grid.ReadOnly = true; 
             grid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            DoFormatGrid(grid, tablename);
+        }
+
+        public static void FormatGridForEdit(DataGridView grid, string tablename)
+        {
+            grid.EditMode = DataGridViewEditMode.EditOnEnter;
+            DoFormatGrid(grid, tablename);
+        }
+
+        private static void DoFormatGrid(DataGridView grid, string tablename)
+        {
+            grid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            grid.RowHeadersWidth = 25;
+            grid.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 14, FontStyle.Bold);
+            grid.ColumnHeadersDefaultCellStyle.WrapMode = DataGridViewTriState.False;
+            foreach (DataGridViewColumn col in grid.Columns)
+            {
+                if (col.Name.EndsWith("Id"))
+                {
+                    col.Visible = false;
+                }
+            }
+            string pkname = tablename + "Id";
+            if (grid.Columns.Contains(pkname))
+            {
+                grid.Columns[pkname].Visible = false;
+            }
+        }
+
+        public static int GetIdFromGrid(DataGridView grid, int rowindex, string columnname)
+        {
+            int id = 0;
+            if(rowindex < grid.Rows.Count && grid.Columns.Contains(columnname) && grid.Rows[rowindex].Cells[columnname].Value != DBNull.Value){
+                if (grid.Rows[rowindex].Cells[columnname].Value is int)
+                {
+                    id = (int)grid.Rows[rowindex].Cells[columnname].Value;
+                }
+                
+            }
+            return id;
+        }
+
+        public static int GetIdFromComboBox(ComboBox lst)
+        {
+            int value = 0;
+            if(lst.SelectedValue != null && lst.SelectedValue is int)
+            {
+                value = (int)lst.SelectedValue;
+            }
+            return value;
+        }
+
+
+        public static void AddComboBoxToGrid(DataGridView grid, DataTable datasource, string tablename, string displaymember)
+        {
+            DataGridViewComboBoxColumn c = new();
+            c.Name = tablename;
+            c.DataSource = datasource;
+            c.DisplayMember = displaymember;
+            c.ValueMember = tablename + "Id";
+            c.DataPropertyName = c.ValueMember;
+            c.HeaderText = tablename;
+            grid.Columns.Insert(0, c);
+        }
+
+        public static void AddDeleteButtonToGrid(DataGridView grid, string deletecolname)
+        {
+            grid.Columns.Add(new DataGridViewButtonColumn() { Text = "X", HeaderText = "Delete", Name = deletecolname, UseColumnTextForButtonValue = true });
+
+        }
+
+        public static bool IsFormOpen(Type formtype, int pkvalue = 0)
+        {
+            bool exists = false;
+            foreach (Form frm in Application.OpenForms)
+            {
+                int frmpkvalue = 0;
+                if(frm.Tag != null && frm.Tag is int)
+                {
+                    frmpkvalue = (int)frm.Tag;
+                }
+                if (frm.GetType() == formtype && frmpkvalue == pkvalue)
+                {
+                    frm.Activate();
+                    exists = true;
+                    break;
+                }
+            }
+            return exists;
+        }
+
+        public static void SetupNav(ToolStrip ts)
+        {
+            ts.Items.Clear();
+            foreach (Form f in Application.OpenForms)
+            {
+                if (f.IsMdiContainer == false)
+                {
+                    ToolStripButton btn = new();
+                    btn.Text = f.Text;
+                    btn.Tag = f;
+                    btn.Click += Btn_Click;
+                    ts.Items.Add(btn);
+                    ts.Items.Add(new ToolStripSeparator());
+                }
+
+            }
+        }
+
+
+        private static void Btn_Click(object? sender, EventArgs e)
+        {
+            if (sender != null && sender is ToolStripButton)
+            {
+                ToolStripButton btn = (ToolStripButton)sender;
+                if (btn.Tag != null && btn.Tag is Form)
+                {
+                    ((Form)btn.Tag).Activate();
+                }
+            }
         }
     }
 }
